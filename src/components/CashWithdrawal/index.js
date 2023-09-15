8ce87603da57c2b60518798658726a71fd778234
BOJJA RAJI <94085980+BOJJRAJI@users.noreply.github.com>
2023-09-15 11:14:21 +0530

    Update index.js

--- src/components/CashWithdrawal/index.js
+++ src/components/CashWithdrawal/index.js
@@ -5,10 +5,8 @@ import './index.css'
 class CashWithdrawal extends Component {
   state = {amount: 2000}
 
-  withdrawCash = value => {
-    this.setState(prevState => ({
-      amount: prevState.amount - value,
-    }))
+  onWithdrawAmount = value => {
+    this.setState(prevState => ({amount: prevState.amount - value}))
   }
 
   render() {
@@ -19,24 +17,24 @@ class CashWithdrawal extends Component {
       <div className="bg-container">
         <div className="card">
           <div className="profile-container">
-            <p className="profile">S</p>
+            <div className="profile-icon-container">S</div>
             <h1 className="profile-name">Sarah Williams</h1>
           </div>
           <div className="balance-container">
             <p className="balance-heading">Your Balance</p>
-            <div className="amount-container">
-              <p className="cash">{amount}</p>
+            <div className="amount-display-container">
+              <p className="amount">{amount}</p>
               <p className="rupees-para">In Rupees</p>
             </div>
           </div>
           <p className="withdraw-heading">Withdraw</p>
-          <p className="sum-para">CHOOSE SUM (IN RUPEES)</p>
-          <ul className="cards-list">
-            {denominationsList.map(eachList => (
+          <p className="choose-option-para">CHOOSE SUM (IN RUPEES)</p>
+          <ul className="amount-lists">
+            {denominationsList.map(item => (
               <DenominationItem
-                itemDetails={eachList}
-                key={eachList.id}
-                withdrawCash={this.withdrawCash}
+                item={item}
+                key={item.id}
+                WithdrawAmount={this.onWithdrawAmount}
               />
             ))}
           </ul>
